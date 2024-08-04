d318d0a7d274f048bbfa1d140d5be2e886b5d82e
Cen-2-rion <atrohov1982@gmail.com>
2024-08-05 01:23:03 +0300

    first commit mocking

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -3,17 +3,27 @@ import getLevel from '../app';
 
 jest.mock('../http'); // сообщаем что этот файл должен стать моком
 
-beforeEach(() => {
-  jest.resetAllMocks(); // сбрасываем моки перед каждым тестом
+// сбрасываем моки после каждого теста
+afterEach(() => {
+  jest.resetAllMocks();
 });
 
+// проверочные тесты
 test('should call fetchData', () => {
   fetchData.mockReturnValue({}); // моким fetchData и вызываем её с аргументом
-  getLevel(1); // вызываем функцию с моковым fetchData
-  expect(fetchData).toHaveBeenCalledWith('https://server/user/1');
+  getLevel(1); // вызываем функцию с userId
+
+  expect(fetchData).toHaveBeenCalledTimes(1);
 });
 
 test('should return level message', () => {
   fetchData.mockReturnValue({ status: 'ok', level: 1 });
+
   expect(getLevel(1)).toBe('Ваш текущий уровень: 1');
+});
+
+test('should return error message when status is not ok', () => {
+  fetchData.mockReturnValue({ status: 'error', level: 1 }); // настраиваем мок fetchData на ошибку
+
+  expect(getLevel(1)).toBe('Информация об уровне временно недоступна');
 });
